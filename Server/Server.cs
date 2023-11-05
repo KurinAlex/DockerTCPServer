1ef2116d54780de5d34feb6b3faeff12347581e4
KurinAlex <olexkurin@gmail.com>
2023-11-05 23:46:05 +0200

    Add Client and Server implementation

--- Server/Server.cs
+++ Server/Server.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+using System.Net;
+
+var server = new TcpListener(IPAddress.Any, 8888);
+
+try
+{
+    server.Start(16);
+    Console.WriteLine("Server started, listening for connections...");
+
+    while (true)
+    {
+        await ProcessClient();
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    server.Stop();
+}
+
+async Task ProcessClient()
+{
+    try
+    {
+        using TcpClient client = await server.AcceptTcpClientAsync();
+        using NetworkStream stream = client.GetStream();
+        using var reader = new BinaryReader(stream);
+
+        string name = reader.ReadString();
+        Console.WriteLine($"{name} connected");
+
+        for (int i = 0; i < 100; i++)
+        {
+            string message = reader.ReadString();
+            Console.WriteLine($"{name}: {message}");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
